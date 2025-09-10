121e5378bc2c19235949075222e41dc02b7498f4
Amatullah05 <theamatullahknw@gmail.com>
2025-09-10 15:51:28 +0530

    Responsive changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="navbar navbar-expand-lg bg-white sticky-top">
+    <header className="navbar navbar-expand-lg bg-white navbar-light sticky-top">
       <div className="container">
         <Link to="/">
           <img src={Logo} alt="Leadassist Logo" height="50" />
@@ -16,6 +16,9 @@ const Header = () => {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
